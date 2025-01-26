a11438f3023636a034a0c53ede34c8370dc9f619
mohammadAinia <mmyyttt@gmail.com>
2025-01-26 20:32:05 +0300

    Updates

--- assets/extends-C67MoaaU.js
+++ assets/extends-C67MoaaU.js
@@ -1,4 +1,4 @@
-import{r as Ye,g as s_,j as In}from"./index-B4k39if2.js";/**
+import{r as Ye,g as s_,j as In}from"./index-Dt-CR42X.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
