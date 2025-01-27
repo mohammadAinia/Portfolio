55a037f65268fa39037ca9e1c10c1fd882cf87df
mohammadAinia <mmyyttt@gmail.com>
2025-01-27 19:09:33 +0300

    Updates

--- assets/extends-DToylXtS.js
+++ assets/extends-DToylXtS.js
@@ -1,4 +1,4 @@
-import{r as Ye,g as s_,j as In}from"./index--OIqely4.js";/**
+import{r as Ye,g as s_,j as In}from"./index-qzr9vEGM.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
